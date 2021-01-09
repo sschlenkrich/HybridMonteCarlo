17b8eee5917b368db5b51bce5a3a74d9a1f437b9
Sebastian Schlenkrich <sebastian.schlenkrich@d-fine.de>
2021-01-09 20:55:48 +0100

    Add AndersenFutureModel

---
+++ hybmc/models/AndersenFutureModel.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+
+import numpy as np
+from scipy.optimize import brentq
+from hybmc.mathutils.Helpers import Black, Bachelier, BachelierImpliedVol
+from hybmc.models.StochasticProcess import StochasticProcess
+
+
+class AndersenFutureModel(StochasticProcess):
+
+    # Python constructor
+    def __init__(self, futuresCurve, kappa, sigma_0, sigma_infty, rho_infty):
+        self._futuresCurve = futuresCurve
+        self._kappa        = kappa
+        self._sigma_0      = sigma_0
+        self._sigma_infty  = sigma_infty
+        self._rho_infty    = rho_infty
+        #
+
+    # model parameter functions
+
+    def h1(self, t):
+        return -self._sigma_infty + self._rho_infty * self._sigma_0
+
+    def h2(self, t):
+        return self._sigma_0 * np.sqrt(1.0 - self._rho_infty**2)
+
+    def hInfty(self):
+        return self._sigma_infty
+    
+    def covariance(self, t, T):
+        h1_ = self.h1(0.5*(t+T))
+        h2_ = self.h2(0.5*(t+T))
+        hi_ = self.hInfty()
+        m11 = (1.0 - np.exp(-2*self._kappa*(T-t)))/2.0/self._kappa * (h1_*h1_ + h2_*h2_)
+        m12 = (1.0 - np.exp(-  self._kappa*(T-t)))/self._kappa * h1_ * hi_
+        m22 = (T-t) * hi_ * hi_
+        return np.array([ [m11, m12], [m12, m22] ])
+
+    def y(self, t):
+        # simple if no time-dependence
+        return self.covariance(0.0, t)
+
+    # path/payoff interfaces
+
+    def futurePrice(self, t, T, X, alias):
+        # F(t,T) = F(0,T) * exp{ ... }
+        # [...]  = h(t,T)^T [z(t) - 0.5 y(t) h(t,T) ]  ... this is wrong
+        h = np.array([ np.exp(-self._kappa*(T-t)), 1.0 ])
+        yt = self.y(t)
+        x2 = X - 0.5 * (yt @ h)
+        XtT = np.dot(h,x2)
+        FtT = np.exp(XtT) # do not forget F(0,T)
+        # FtT *= futuresCurve.future(T)
+        return FtT
+
+    # process simulation interface
+    # X = [ z1, z2 ] (y calculated, no numeraire)
+
+    def size(self):   # dimension of X(t) = [ x ]
+        return 2
+
+    def factors(self):   # dimension of W(t)
+        return 2
+
+    def initialValues(self):
+        return np.zeros(2)
+
+    def evolve(self, t0, X0, dt, dW, X1):
+        cov = self.covariance(t0,t0+dt)
+        L = np.linalg.cholesky(cov)
+        H = np.array([ np.exp(-self._kappa*dt), 1.0 ])
+        G = np.array([ (1.0 - np.exp(-self._kappa*dt))/self._kappa, dt ])
+        theta = np.zeros(2)  # incorporate quanto adjustment here
+        # H(t0,t1) x0 + G(t0,t1) theta
+        for i in range(2):
+            X1[i] = H[i] * X0[i] + G[i] * theta[i]
+        # add diffusion term        
+        for i in range(2):
+            for j in range(2):
+                X1[i] += L[i,j] * dW[j]  # maybe also exploit that L is lower triangular
+        # done
---
+++ test/unittests/TestAndersenFutureModel.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('./')
+
+import unittest
+
+import numpy as np
+from hybmc.models.AndersenFutureModel import AndersenFutureModel
+from hybmc.models.MarkovFutureModel import MarkovFutureModel
+
+from hybmc.simulations.McSimulation import McSimulation
+
+import matplotlib.pyplot as plt
+
+class TestAffineShortRateModel(unittest.TestCase):
+
+    def test_ModelSetup(self):
+        # Andersen example
+        kappa       = 1.35
+        sigma_0     = 0.50
+        sigma_infty = 0.17
+        rho_infty   = 0.50
+        model = AndersenFutureModel(None,kappa,sigma_0,sigma_infty,rho_infty)
+        #print(model.h1(0.0))
+        #print(model.h2(0.0))
+        #print(model.hInfty())
+        self.assertAlmostEqual(model.h1(0.0),0.08,16)
+        self.assertAlmostEqual(model.h2(0.0),0.4330127018922193,16)
+        self.assertAlmostEqual(model.hInfty(),0.17,16)
+
+    def test_MartingaleProperty(self):
+        kappa       = 1.35
+        sigma_0     = 0.50
+        sigma_infty = 0.17
+        rho_infty   = 0.50
+        model = AndersenFutureModel(None,kappa,sigma_0,sigma_infty,rho_infty)
+        #
+        times = np.linspace(0.0, 5.0, 3)
+        nPaths = 2**13
+        seed = 14159265359
+        sim = McSimulation(model,times,nPaths,seed,False,showProgress=False)
+        #
+        for idx in range(1,times.shape[0]):
+            t = times[idx]
+            for dT in [ 0.0, 1.0, 2.0, 5.0, 10.0]:
+                T = t + dT
+                F = np.array([ model.futurePrice(t,T,X,None) for X in sim.X[:,idx,:] ])
+                Fav = np.mean(F)
+                sigma = np.std(F) / np.sqrt(t)
+                # print('t: %6.2f, T: %6.2f, F: %6.4f, sigma: %6.4f' % (t,T,Fav,sigma) )
+                # print(np.abs(Fav - 1.0))
+                self.assertLess(np.abs(Fav - 1.0),0.0026)
+
+    def test_CompareWithMarkovModel(self):
+        kappa       = 1.35
+        sigma_0     = 0.50
+        sigma_infty = 0.17
+        rho_infty   = 0.50
+        model0 = AndersenFutureModel(None,kappa,sigma_0,sigma_infty,rho_infty)
+        #
+        def sigmaT(sigma_0, sigma_infty, rho_infty):
+            h1 = -sigma_infty + rho_infty * sigma_0
+            h2 = sigma_0 * np.sqrt(1.0 - rho_infty**2)
+            hi = sigma_infty
+            return np.array([ [h1, h2], [hi, 0.0] ])
+        #
+        def chi(kappa):
+            return np.array([ kappa, 0.0 ])
+        #
+        sigmaT_ = sigmaT(sigma_0,sigma_infty,rho_infty)
+        chi_    = chi(kappa)
+        #
+        d = 2
+        times = np.array([0.0])
+        model1 = MarkovFutureModel(None,d,times,np.array([ sigmaT_ ]),chi_)
+        #
+        times = np.linspace(0.0, 5.0, 3)
+        nPaths = 2**3
+        seed = 14159265359
+        #
+        sim0 = McSimulation(model0,times,nPaths,seed,False,showProgress=False)
+        sim1 = McSimulation(model1,times,nPaths,seed,False,showProgress=False)
+        #
+        for idx in range(1,times.shape[0]):
+            t = times[idx]
+            for dT in [ 0.0, 1.0, 2.0, 5.0, 10.0]:
+                T = t + dT
+                F0 = np.array([ model0.futurePrice(t,T,X,None) for X in sim0.X[:,idx,:] ])
+                F1 = np.array([ model1.futurePrice(t,T,X,None) for X in sim1.X[:,idx,:] ])
+                # print(F0-F1)
+                self.assertLess(np.max(np.abs(F0-F1)),3.0e-16)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
